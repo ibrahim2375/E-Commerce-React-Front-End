b667fa16145779c1d31659bf8bebd0c0098af5d9
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-02-14 00:53:28 +0200

    up router

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { AnimatePresence, motion } from "framer-motion";
 //import Lyout general design
 import Layout from "./components/Layouts/Layout";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
+              <Route path="products" element={<Products />} />
               <Route path="login" element={<Login />} />
               <Route path="register" element={<Register />} />
               <Route path="cart" element={<Cart />} />
