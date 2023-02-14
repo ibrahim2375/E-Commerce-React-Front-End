025938e3d6209edae1405d8d8ba2f5fe197833ec
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-02-14 21:18:42 +0200

    update

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from "react";
-//api request
-import axios from "../api/axios";
+//hook
+import UseFetch from "../Hooks/UseFetch";
 //components
 import Advertisement from "../components/Layouts/Advertisement";
 import ProductsComponent from "../components/Ptoducts/ProductsComponent";
 import ProductsFilter from "../components/Ptoducts/ProductsFilter";
 function Products() {
-  const [products, setProducts] = useState([]);
-
   //get all products
-  useEffect(() => {
-    const getProducts = async () => {
-      await axios
-        .get("/products")
-        .then((response) => setProducts(response.data));
-    };
-    getProducts();
-  }, []);
+  const { data, loading, error } = UseFetch("/products");
   return (
     <div className="products container mx-auto px-4">
       <Advertisement />
       <ProductsFilter />
-      <ProductsComponent products={products} />
+      <ProductsComponent products={data} />
     </div>
   );
 }
