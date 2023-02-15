9775d8033098e80bf6bb37c8e1e6a585caacbb8d
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2023-02-15 18:50:02 +0200

    update  data fetch

--- src/components/Products/ProductsComponent.jsx
+++ src/components/Products/ProductsComponent.jsx
@@ -8,15 +8,15 @@ import ReactPaginate from "react-paginate";
 //css
 import "../../css/Products/ProductsComponent.css";
 
-function ProductsComponent({ products, filterdProducts, loading }) {
+function ProductsComponent({ filterdProducts, loading }) {
   // const products = useSelector((state) => state.products);
   // handle paginations variable
   const [pageNumber, setPageNumber] = useState(0);
-  const productsPerPages = 20;
+  const productsPerPages = 10;
   const productsVisited = pageNumber * productsPerPages;
-  let pageCount = Math.ceil(products?.length / productsPerPages);
+  let pageCount = Math.ceil(filterdProducts?.length / productsPerPages);
   //get current products even its filterd or all products and get current products
-  var currentProducts = products?.slice(
+  var currentProducts = filterdProducts?.slice(
     productsVisited,
     productsVisited + productsPerPages
   );
@@ -32,27 +32,16 @@ function ProductsComponent({ products, filterdProducts, loading }) {
         {loading && (
           <h1 className="w-16 h-16 rounded-full border-1 border-l-4  border-solid border-l-gray-400 border-gray-200 animate-spin"></h1>
         )}
-        {filterdProducts?.length !== 0
-          ? filterdProducts?.map((product) => (
-              <Product
-                key={product?.id}
-                id={product?.id}
-                img={product?.img}
-                name={product?.name}
-                price={product?.price}
-                quantity={product?.quantity}
-              />
-            ))
-          : currentProducts?.map((product) => (
-              <Product
-                key={product?.id}
-                id={product?.id}
-                img={product?.img}
-                name={product?.name}
-                price={product?.price}
-                quantity={product?.quantity}
-              />
-            ))}
+        {currentProducts?.map((product) => (
+          <Product
+            key={product?.id}
+            id={product?.id}
+            img={product?.img}
+            name={product?.name}
+            price={product?.price}
+            quantity={product?.quantity}
+          />
+        ))}
       </div>
       {/* pagination links  */}
       <div className="pagination my-20">
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,6 +4,7 @@ import UseFetch from "../Hooks/UseFetch";
 //redux
 import * as Actions from "../redux/reducers";
 import { useSelector } from "react-redux";
+
 //components
 import Advertisement from "../components/Layouts/Advertisement";
 import ProductsComponent from "../components/Products/ProductsComponent";
@@ -11,7 +12,7 @@ import ProductsFilter from "../components/Products/ProductsFilter";
 function Products() {
   // const products = useSelector((state) => state.products);
   const [filterdProducts, setFilterdProducts] = useState([]);
-  const [filters, setFilters] = useState({});
+  const [filters, setFilters] = useState(null);
   //get all products
   const { data, loading, error } = UseFetch("/products", Actions.Products);
   //get fillters values
@@ -20,26 +21,38 @@ function Products() {
   };
   //filter products
   useEffect(() => {
-    // get all filtered
-    filters &&
-      setFilterdProducts(
-        data?.filter(
-          (product) =>
-            // product?.avilableSizes.includes(filters?.Size) ||
-            product?.avilableColors?.includes(filters?.Color) ||
-            product?.category === filters?.Type
-        )
-      );
+    // get all  data if ther is no filters or get data depend on filters
+    setFilterdProducts(
+      filters === null
+        ? data
+        : data
+            ?.filter(
+              (product) =>
+                product?.avilableSizes?.includes(filters?.Size) ||
+                product?.avilableColors?.includes(filters?.Color) ||
+                product?.category === filters?.Type
+            )
+            .sort((a, b) =>
+              filters?.SortBy === "asc"
+                ? a?.price - b?.price
+                : filters?.SortBy === "dec"
+                ? b?.price - a?.price
+                : null
+            )
+    );
   }, [data, filters]);
   return (
     <div className="products container mx-auto px-4">
       <Advertisement />
       <ProductsFilter onChange={FilterValuesOnChange} />
-      <ProductsComponent
-        products={data}
-        loading={loading}
-        filterdProducts={filterdProducts}
-      />
+      {error ? (
+        <h1>There is a problem try again</h1>
+      ) : (
+        <ProductsComponent
+          loading={loading}
+          filterdProducts={filterdProducts}
+        />
+      )}
     </div>
   );
 }
